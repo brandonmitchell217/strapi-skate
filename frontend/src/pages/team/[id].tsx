e67ff53bda29d397ac93ce7344ce19c2ec0b52d3
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-02-24 21:52:15 -0500

    team member page

--- frontend/src/pages/team/[id].tsx
+++ frontend/src/pages/team/[id].tsx
@@ -1,15 +1,25 @@
 import React from "react";
-import { fetchQuery } from "../../lib/util";
+import { base, fetchQuery } from "../../lib/util";
 import Layout from "../../layouts/Layout";
 import { TeamMembersProps, Props } from "../../lib/types";
+import Image from "next/image";
 
 export default function ItemPage({ member }: any) {
   console.log(member);
   return (
     <Layout>
-      <div>
-        <h1>{member.data.attributes.name}</h1>
-        <p>{member.data.attributes.hometown}</p>
+      <div className="max-w-4xl m-auto card lg:card-side bg-base-100 shadow-xl rounded-2xl">
+        <Image
+          src={`${base}${member.data.attributes.image.data.attributes.url}`}
+          alt="dadfadfa dfa"
+          width={350}
+          height={350}
+          className="rounded-tl-2xl rounded-bl-2xl"
+        />
+        <div className="card-body">
+          <h2 className="card-title">{member.data.attributes.name}</h2>
+          <p>{member.data.attributes.hometown}</p>
+        </div>
       </div>
     </Layout>
   );
